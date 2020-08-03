da56c2bb792536f4ee6c650112266c34ef79472b
Clyde Tedrick <iamclyde@gmail.com>
2020-08-03 03:03:35 -0400

    issue3: first cut integration

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,90 +1,30 @@
 import { Component , OnInit } from '@angular/core';
-import { ToastController } from '@ionic/angular';
-import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
-import { WebsocketService } from '../websocket.service';
 import { ColorsrvService } from '../colorsrv.service';
 
-import * as Rx from 'rxjs/Rx';
-import { Observable, Subject } from "rxjs/Rx";
-import {interval} from "rxjs/internal/observable/interval";
-import {startWith, switchMap} from "rxjs/operators";
-
-export interface Vote {
-  action: string;
-  color: string;
-
-}
-export interface VoteResult {
-  r: string;
-  g: string;
-  b: string;
-  hex: string;
-
-}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 
-
 export class HomePage implements OnInit {
-  changeColor = 'success';
-  colors: string[] = ['#000000', '#db0f0f', '#0fbf0f', '#35a3e8', '#FFFFFF'];
-  color: string[] = ['yellow', 'blue', 'red', '', '#FFFFFF'];
-  clr = '#D3D3D3';
-  currentColor: string = this.colors[1];
-  message = '';
-  //messages = [];
-  currentUser = '';
-  subject: Rx.Subject<VoteResult>;
-  subjectVote: Rx.Subject<Vote>;
-  url = 'wss://ws.colorchooser.iamcly.de';
-  messages: string[] = [];
+  clr = '#555555';
 
   constructor( private colorsrvService: ColorsrvService) {
-    this.changeColor = 'my-special-color';
-    }
-
-  ngOnInit(){
-
-    this.messages = this.colorsrvService.messages;
-    console.log('Response from websocket: ' + this.messages);
-    /*this.colorsrvService.voteResults.subscribe(msg => {
-      console.log('Response from websocket: ' + msg);
-    });*/
-
-
+    colorsrvService.getSubject().subscribe(evt => {
+      if (evt.data) {
+        let payload = JSON.parse(evt.data);
+        this.clr = payload.hex;
+      }
+    });
   }
-  changeColors1(cl){
-    console.log(cl);
-    if ( cl === 'r'){
-      console.log('red');
-      this.clr = '#f53d3d' ;
-    }
-    if ( cl === 'g'){
-      console.log('green');
 
-      this.clr = '#32db64' ;
-     }
-    if ( cl === 'b' ){
-      console.log('red');
-      this.clr = '#488aff' ;
-     }
+  ngOnInit(){}
 
-  }
-  switchColor(index: number) {
-    this.currentColor = this.colors[index];
-  }
   changeColors(cl){
-
-    const val = {
+    this.colorsrvService.sendMessage(JSON.stringify({
       action: 'vote',
       color: cl
-    };
-    //this.colorsrvService.vote.next(val);
-
-    console.log('new message from client to websocket: ', cl);
-
+    }));
   }
 }
--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,24 +1,17 @@
 import { Injectable } from '@angular/core';
-// import * as Rx from 'rxjs/Rx';
 import {Observable} from 'rxjs/Observable';
 import {Subject} from 'rxjs/Subject';
 import {Observer} from 'rxjs/Observer';
-
 
 @Injectable({
   providedIn: 'root'
 })
 
-
 export class WebsocketService {
-// private subject: Rx.Subject<MessageEvent>;
-private subject: Subject<any> ;
-
-private ws: WebSocket;
-
+  private subject: Subject<any>;
+  private ws: WebSocket;
   constructor() { }
 
- // public connect(url): Rx.Subject<MessageEvent> {
   public connect() {
     if (!this.subject) {
       this.subject = this.create();
@@ -28,38 +21,33 @@ private ws: WebSocket;
     }
     return this.subject;
   }
-  // public create(url): Rx.Subject<MessageEvent> {
-    public create() {
-     this.ws = new WebSocket('wss://ws.colorchooser.iamcly.de/');
-    // tslint:disable-next-line: only-arrow-functions
-     this.ws.onerror = function(error) {
+
+  public create() {
+    this.ws = new WebSocket('wss://ws.colorchooser.iamcly.de/');
+    this.ws.onerror = function(error) {
       console.log('WebSocket Error: ' + error);
     };
-    // tslint:disable-next-line: only-arrow-functions
-     this.ws.onopen = function(open) {
+
+    this.ws.onopen = function(open) {
       console.log('WebSocket open: ' + open);
-     // console.log('WebSocket readt: ' + this.ws.readyState);
     };
-    // tslint:disable-next-line: only-arrow-functions
-     this.ws.onmessage = function(event) {
+
+    this.ws.onmessage = function(event) {
       console.log('WebSocket onmessage: ' + event);
-      // console.log('WebSocket on message status: ' + this.ws.readyState);
     };
-    // tslint:disable-next-line: deprecation
-     const observable = Observable.create(
+
+    const observable = Observable.create(
       (obs: Observer<MessageEvent>) => {
         this.ws.onmessage = obs.next.bind(obs);
-        this.ws.onerror = obs.error.bind(obs);
-        // ws.onmessage = obs.next.bind(obs);
-        this.ws.onclose = obs.complete.bind(obs);
-        // return ws.onopen.bind(ws);
+        this.ws.onerror   = obs.error.bind(obs);
+        this.ws.onclose   = obs.complete.bind(obs);
         return this.ws.close.bind(this.ws);
       }
     );
-     console.log('open' , this.ws.readyState );
 
-     const observer  = {
-      // tslint:disable-next-line: ban-types
+    console.log('open' , this.ws.readyState );
+
+    const observer = {
       next: (data: Object) => {
         console.log('open' , this.ws.readyState );
 
@@ -71,9 +59,10 @@ private ws: WebSocket;
         }
       }
     };
-    // tslint:disable-next-line: deprecation
-     return Subject.create(observer, observable );
+
+    return Subject.create(observer, observable);
   }
+  
   sendMessage(message: string) {
     console.log(message);
     this.ws.send(message);
